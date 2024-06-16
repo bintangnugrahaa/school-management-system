fdf9630d6e48f9f3e26f27d0ab4e07ef24472ba3
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2024-06-17 01:15:28 +0700

    feat(auth): setup login and register pages
    - Created login blade template with form fields for email and password.
    - Created register blade template with form fields for name, email, password, and password confirmation.
    - Implemented authentication logic using Laravel's built-in Auth system.
    - Added validation for login and registration forms.
    - Integrated error handling for invalid login attempts and registration errors.
    - Styled pages with Bootstrap for a consistent look and feel.
    
    This commit introduces the setup of the login and register pages, allowing users to authenticate and create new accounts within the school management system.
    
    Related ticket: #9

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
     public function index()
     {
         return view('admin.login');
+    }
+    public function authenticate(Request $req)
+    {
+        $req->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        if (Auth::guard('admin')->attempt(['email' => $req->email, 'password' => $req->password])) {
+            echo "ok";
+        } else {
+            return redirect()->route('admin.login')->with('error', 'Email or Password is incorrect');
+        }
+    }
+    public function register()
+    {
+        $user = new User();
+        $user->name = 'Admin';
+        $user->role = 'admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make('admin');
+        $user->save();
+        return redirect()->route('admin.login')->with('success', 'User registered successfully');
     }
     public function dashboard()
     {
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/', function () {
 });
 
 Route::get('admin/login', [AdminController::class, 'index'])->name('admin.login');
+Route::get('admin/register', [AdminController::class, 'register'])->name('admin.register');
+Route::post('admin/login', [AdminController::class, 'authenticate'])->name('admin.authenticate');
 Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 Route::get('admin/form', [AdminController::class, 'form'])->name('admin.form');
 Route::get('admin/table', [AdminController::class, 'table'])->name('admin.table');
